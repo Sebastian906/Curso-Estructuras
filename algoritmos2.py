2bf7a5803b0208550471f38e4fde8f76f90f3426
Sebastian906 <sebastian.1701811396@ucaldas.edu.co>
2025-05-30 18:51:55 -0500

    Creación de método de append para revertir valores de una lista

---
+++ algoritmos2.py
@@ -0,0 +1,11 @@
+lista = ["g", "f", "e", "d", "c", "b", "a"]
+nueva_lista = []
+
+# indice = 6
+# nueva_lista = [lista[6]] = ["a"]
+# indice = 5
+# nueva_lista = ["a", lista[5]] = ["a", "b"]
+for indice in range(len(lista)-1, -1, -1):
+    nueva_lista.append(lista[indice])
+
+print(nueva_lista)
