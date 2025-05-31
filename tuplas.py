e03ef6d969868ebf308af3706686338769a2beb1
Sebastian906 <sebastian.1701811396@ucaldas.edu.co>
2025-05-31 17:57:07 -0500

    Se agrega clase donde se incluyen tuplas en python

---
+++ tuplas.py
@@ -0,0 +1,13 @@
+tupla = ("a", "b", "c")
+
+tupla2 = 1, 2, 3, 4
+
+tupla_combinada = ("a", 2, "c", 4.4)
+
+print(tupla_combinada)
+
+print(tupla_combinada[0])
+
+tupla_grande = ("Python", "Java", "C++", "Javascript", "Go", "PHP")
+print(tupla_grande)
+print(tupla_grande[2:5])
