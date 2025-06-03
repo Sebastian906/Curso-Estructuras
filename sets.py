7f101945db6cce99b04288865f04190b03cc5773
Sebastian906 <sebastian.1701811396@ucaldas.edu.co>
2025-06-03 10:49:14 -0500

    Creación de archivo de sets

---
+++ sets.py
@@ -0,0 +1,10 @@
+set1 = (2, 3, 5, 7)
+set2 = set([2, 3, 5, 7])
+
+nombres = {"Elena", "Juana", "Pablo", "Elena"}
+print(nombres)
+
+for nombre in nombres:
+    print(nombre)
+
+print("Elena" in nombres)
