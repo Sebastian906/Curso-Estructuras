5bdef118a9bb760fb4445ff801797a02c33ad3e0
Sebastian906 <sebastian.1701811396@ucaldas.edu.co>
2025-06-06 16:34:33 -0500

    Se usa la función intersection para mostrar las coincidencias entre los conjuntos

--- sets_funciones.py
+++ sets_funciones.py
@@ -1,5 +1,8 @@
-lenguajes1 = {"Python", "Java", "PHP"}
-lenguajes2 = {"C++", "Javascript", "Java"}
+lenguajes1 = {"Python", "Java", "PHP", "Go"}
+lenguajes2 = {"C++", "Javascript", "Java", "C#"}
 
 todos_lenguajes = lenguajes1.union(lenguajes2)
 print(todos_lenguajes)
+
+coincidencias = lenguajes1.intersection(lenguajes2)
+print(coincidencias)
